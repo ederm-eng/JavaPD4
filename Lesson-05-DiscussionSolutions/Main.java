f2604bec71bf0f0b5f5c1c5759b35f60f984a3ca
ederm-eng <ederm@nycstudents.net>
2025-09-17 14:15:21 +0000

    sadoijasd

---
+++ Lesson-05-DiscussionSolutions/Main.java
@@ -0,0 +1,34 @@
+class Main {
+  
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  void init(){
+
+  
+    
+    //integer division truncates the decimal place 
+    //and only get the integer part
+    int solut1 = 1/2
+    System.out.println(solut1);
+    int solut2 = 3/2
+    System.out.println(solut2);
+    //How to avoid
+    System.out.println(1.0/2);
+    System.out.println(1/2.0);
+  
+    System.out.println("********************");
+    //Modulus operator or remainder operator %
+    System.out.println(1%2);
+    System.out.println(3%2);
+    System.out.println(7%3);
+    System.out.println(5%2);
+    System.out.println(6%2);
+   
+
+
+  }
+
+  
+}
