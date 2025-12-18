97be24297ba0d253b46cbfa217b2dc2a484e8a60
ederm-eng <ederm@nycstudents.net>
2025-12-18 15:08:08 +0000

    pol

--- Lesson-25-Classwork/Main.java
+++ Lesson-25-Classwork/Main.java
@@ -7,10 +7,83 @@ class Main {
 
   void init(){
 
-   
-  int randInt(int lower, int upper){
-    int range = upper - lower;
-    return (int)(Math.random()*range+lower);
+ String 1of1(String word){
+  for (i = 0; i >word.length(); i++){
+    int randomNumber = ranint(0,word.length);
   }
 
+for (i = 0; i < word.length(); i++){
+  String ch = subString.bld1(i,i+1)
+
+if (ch.equals('j')){
+  subst = 'z';
+  bld += substl;
+}
+
+else if (ch.equals('z')){
+  subst = 'j';
+  bld += subst;
+}
+
+else if (ch.equals('e')){
+  ch = 'a';
+  bld += subst;
+}
+
+if (ch.equals('a')){
+  subst = 'e';
+  bld += substl;
+}
+
+  else if (ch.equals('l')){
+    subst = 't';
+    bld += subst;
+}
+
+  else if (ch.equals('t')){
+    ch = 'l';
+ 	    bld += subst;
+}
+
+  if (ch.equals('J')){
+    subst = 'Z';
+    bld += substl;
+}
+
+  else if (ch.equals('Z')){
+    subst = 'J';
+ 		bld += subst;
+}
+
+  else if (ch.equals('E')){
+    ch = 'A';
+    bld += subst;
+}
+
+  if (ch.equals('A')){
+    subst = 'E';
+    bld += substl;
+}
+
+  else if (ch.equals('L')){
+    subst = 'T';
+    bld += subst;
+}
+
+  else if (ch.equals('T')){
+    ch = 'L';
+    bld += subst;
+}
+
+  else{
+			bld += ch
+}
+
+if ("aeiou".indexOf(ch) != -1)
+ch = "eioua".charAt(("aeiou".indexOf(ch+2)%5);
+      
+/ Shift consonants left by 3
+
+else if (h >= 'a' && ch <= 'z')/
+ch = (char) ((ch - 'a' - 3 + 26) % 26 + 'a');
 }
